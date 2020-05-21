e591449f5a622e3ced3afce3b31cf2254a886953
unknown <lisbiel@gmail.com>
2020-05-21 15:13:41 -0300

    Commit Inicial, ProAgil de Eventos

---
+++ ProAgil.WebAPI/Model/Evento.cs
@@ -0,0 +1,12 @@
+namespace ProAgil.WebAPI.Model
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string Data { get; set; }
+        public string Tema { get; set; }
+        public int QtdePessoas { get; set; }
+        public string Lote { get; set; }
+    }
+}
